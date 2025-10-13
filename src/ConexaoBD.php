080d72ec3cce2e805a7df3d2df5ecf8e0e908166
Mateus Pasa <sal@gmail.com>
2025-10-13 09:11:24 -0300

    conect

--- src/ConexaoBD.php
+++ src/ConexaoBD.php
@@ -2,25 +2,10 @@
 class ConexaoBD{
 
     public static function conectar():PDO{
-        $host = "sql10.freesqldatabase.com";
-        $port = "3306";
-        $dbname = "sql10801235";
-        $username = "sql10801235";
-        $password = "a7MdizUutz";
-        
-        $conexao = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-        
-        // Configurar para lançar exceções em caso de erro
-        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+        $conexao = new PDO("mysql:host=localhost:3306;dbname=redesocial","root","");
         return $conexao;
     }
 }
 
-// Testar a conexão
-try {
-    $conexao = ConexaoBD::conectar();
-    echo "Conexão bem-sucedida!";
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-}
+
+ConexaoBD::conectar();
