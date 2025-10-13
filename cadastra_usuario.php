3416945f06b0ef642bb8082c41b9e1777bfac9e9
Mateus Pasa <sal@gmail.com>
2025-10-13 09:25:22 -0300

    index

--- cadastra_usuario.php
+++ cadastra_usuario.php
@@ -7,14 +7,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
    
     $caminhoFoto = null;
-    if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] == 0) {
+    if (isset($_FILES['foto']) && $_FILES['foto']['error'] == 0) {
         $pasta = "uploads/";
         if (!is_dir($pasta)) {
             mkdir($pasta, 0777, true);
         }
-        $nomeArquivo = uniqid() . "_" . basename($_FILES['imagem']['name']);
+        $nomeArquivo = uniqid() . "_" . basename($_FILES['foto']['name']);
         $caminhoFoto = $pasta . $nomeArquivo;
-        move_uploaded_file($_FILES['imagem']['tmp_name'], $caminhoFoto);
+        move_uploaded_file($_FILES['foto']['tmp_name'], $caminhoFoto);
     }
 
     $dados = [
